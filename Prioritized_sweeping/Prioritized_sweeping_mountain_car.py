59a62558e2f4c1ebc7f6b39b33fa181aff8cd217
Saurabh Bajaj <saurabh.rb440@gmail.com>
2022-12-10 12:31:15 -0500

    moving code inside folder

--- Prioritized_sweeping/Prioritized_sweeping_mountain_car.py
+++ Prioritized_sweeping/Prioritized_sweeping_mountain_car.py
@@ -74,6 +74,36 @@ class PrioritizedSweeping:
         else:
             return -1
     
+    def estimate_J(self, N):
+        G = 0
+        T = 0
+        count = 0
+        for i in range(N):
+            t = self.runEpisode()
+            if t < 500:
+                T += t
+                count += 1
+            # G += reward
+        return T/count if count > 0 else 0.0
+    
+    def runEpisode(self, ):
+        reward = 0
+        state = self.d0()
+        # t= 0.0
+        for t in range(1000):
+            action = self.pi_func(self.test_pol, state)
+            # print(action)
+            R = self.reward_function(state)
+            next_state = self.trans_func(state, action)
+            reward += (self.gamma**t) * R
+            if next_state[0] == 0.5:
+                break
+            state = next_state
+            t += 1.0
+        # print("t = {}".format(t))
+        return t
+
+
     def e_soft_policy_update(self, s, eps):
         row = np.digitize([s[0]], self.bins_x)[0]
         col = np.digitize([s[1]], self.bins_v)[0]
@@ -192,9 +222,9 @@ class PrioritizedSweeping:
         prev_v_list = []
         num_steps_list = []
         while True:
-            print(count)
+            # print(count)
             count += 1
-            alpha = max(alpha*0.999, 0.0001)
+            # alpha = max(alpha*0.999, 0.0001)
             num_steps = 0
             if len(prev_v_list) > running_average_length - 1:
                 prev_v_list.pop(0)
@@ -227,10 +257,10 @@ class PrioritizedSweeping:
                     r1, s_prime1 = self.get_rs_from_model(s1, a1)
                     self.update_q(s1, s_prime1, a1, r1, alpha)
 
-                    eps=max(eps*0.999, 0.0001)
+                    # eps=max(eps*0.999, 0.0001)
 
-                    self.e_soft_policy_update(s=s1, eps=eps)
-                    # self.softmax_policy_update(s=s1, sigma=count)
+                    # self.e_soft_policy_update(s=s1, eps=eps)
+                    self.softmax_policy_update(s=s1, sigma=count)
 
                     for s_, a_ in self.leading_to_s(s1):
                         r_, _ = self.get_rs_from_model(s_, a_)
@@ -255,93 +285,103 @@ class PrioritizedSweeping:
 
 def main():
     gamma = 0.9
-    alpha = 0.01
+    alpha = 0.05
     delta = 0.1
     sigma = 1
-    num_bins = 5
-    eps = 0.01
+    num_bins = 4
+    eps = 0.05
 
-    # num_acts_mean_list = []
-    # num_steps_mean = []
-    # for _ in tqdm(range(20)):
-    #     ql = QLearning(alpha=alpha, gamma=gamma, delta=delta, num_bins=num_bins)
-    #     count, num_episodes_list, num_actions_list, num_steps_list = ql.qlearning(eps=eps)
-
-    #     plt.figure(0)
-    #     plt.plot(num_actions_list, num_episodes_list, 'c')
-    #     plt.title("Learning curve")
-    #     plt.xlabel("Number of Steps till now")
-    #     plt.ylabel("Episodes")
-    #     num_acts_mean_list.append(num_actions_list)
-
-    #     # plt.figure(1)
-    #     # plt.plot(num_episodes_list, num_steps_list, 'c')
-    #     # plt.title("Learning curve")
-    #     # plt.xlabel("Time Steps")
-    #     # plt.ylabel("Episodes")
-    #     num_steps_mean.append(num_steps_list)
-
-    # column_average = [sum(sub_list) / len(sub_list) for sub_list in zip(*num_acts_mean_list)]
-
-    # plt.figure(0)
-    # plt.plot(column_average, num_episodes_list, 'k')
-    # plt.title("Learning curve")
-    # plt.xlabel("Number of Steps till now")
-    # plt.ylabel("Episodes")
-    # # eight = mlines.Line2D([], [], color='c', marker='s', ls='', label='')
-    # nine = mlines.Line2D([], [], color='k', marker='s', ls='', label='mean')
-    # plt.legend(handles=[nine])
-
-    # column_average_steps = [sum(sub_list) / len(sub_list) for sub_list in zip(*num_steps_mean)]
-    # column_variance_steps = [statistics.stdev(i) for i in zip(*num_steps_mean)]
-
-    # plt.figure(1)
-    # plt.plot(num_episodes_list, column_average_steps, 'k')
-    # # plt.fill_between(column_average_steps, list(map(float.__add__, column_average_steps, column_variance_steps)), list(map(float.__sub__, column_average_steps, column_variance_steps)), facecolor='blue', alpha=0.5)
-    # plt.errorbar(num_episodes_list, column_average_steps, yerr=column_variance_steps, elinewidth=0.75, linestyle='None')
-    # plt.title("Learning curve")
-    # plt.ylabel("Number of Steps")
-    # plt.xlabel("Episodes")
-    # # eight = mlines.Line2D([], [], color='c', marker='s', ls='', label='')
-    # nine = mlines.Line2D([], [], color='k', marker='s', ls='', label='mean')
-    # plt.legend(handles=[nine])
-
-    # plt.show()
-
-
-    gamma = 0.9
-    alpha = 0.1
-    delta = 0.1
-    sigma = 1
-    threshold = 0.0 #1e-9
-    eps = 0.1
-    n_iters = 5
-    break_iters = 20
+    # gamma = 0.9
+    # alpha = 0.1
+    # delta = 0.1
+    # sigma = 1
+    threshold = 1e-5
+    # eps = 0.1
+    n_iters = 10
+    break_iters = 60
     running_average_length = 200
 
-    best_threshold = None
-    best_alpha = None
-    best_eps = None
-    best_sigma = None
-    best_v = None
-    best_n_iters = None
-    best_max_norm = float("inf")
-    for best_sigma in np.arange(1, 2, 1):
-        ps = PrioritizedSweeping(alpha=alpha, gamma=gamma, delta=delta, num_bins=num_bins)
-        count, num_episodes_list, num_actions_list, num_steps_list = ps.prio_sweep(eps=eps, threshold=threshold, n_iters=n_iters, break_iters=break_iters, running_average_length=running_average_length)
-    #     if best_max_norm > max_norm:
-    #         best_max_norm = max_norm
-    #         best_threshold = threshold
-    #         best_sigma = sigma
-    #         best_alpha = alpha
-    #         best_eps = eps
-    #         best_v = ps.v
-    #         best_n_iters = n_iters
-    #         best_break_iters = break_iters
-    #         ps.print_policy()
-    #     # print(ps.v)
-    # print("alpha = {}, eps = {}, threshold = {}, sigma= {}, n_iters = {}, break_iters = {} max_norm = {}".format(best_alpha, best_eps, best_threshold, best_sigma, best_n_iters, break_iters, best_max_norm))
-    # print(best_v)
+    for num_bins in range(4, 5):
+        num_acts_mean_list = []
+        num_steps_mean = []
+        J = []
+        for _ in tqdm(range(2)):
+            ps = PrioritizedSweeping(alpha=alpha, gamma=gamma, delta=delta, num_bins=num_bins)
+            count, num_episodes_list, num_actions_list, num_steps_list = ps.prio_sweep(eps=eps, threshold=threshold, n_iters=n_iters, break_iters=break_iters, running_average_length=running_average_length)
+            time = ps.estimate_J(20)
+            if time > 0.0:
+                J.append(time)
+            # print()
+            # plt.figure(0)
+            # plt.plot(num_actions_list, num_episodes_list, 'c')
+            # plt.title("Learning curve")
+            # plt.xlabel("Number of Steps till now")
+            # plt.ylabel("Episodes")
+            num_acts_mean_list.append(num_actions_list)
+
+            # plt.figure(1)
+            # plt.plot(num_episodes_list, num_steps_list, 'c')
+            # plt.title("Learning curve")
+            # plt.xlabel("Time Steps")
+            # plt.ylabel("Episodes")
+            num_steps_mean.append(num_steps_list)
+
+        column_average = [sum(sub_list) / len(sub_list) for sub_list in zip(*num_acts_mean_list)]
+
+        plt.figure(0)
+        plt.plot(column_average, num_episodes_list, label=str(int(num_bins)))
+        # plt.plot(column_average, num_episodes_list, 'k')
+        plt.title("Learning curve")
+        plt.xlabel("Number of Steps till now")
+        plt.ylabel("Episodes")
+        # eight = mlines.Line2D([], [], color='c', marker='s', ls='', label='')
+        # nine = mlines.Line2D([], [], color='k', marker='s', ls='', label='mean')
+        # plt.legend(handles=[nine])
+        plt.legend()
+
+        column_average_steps = [sum(sub_list) / len(sub_list) for sub_list in zip(*num_steps_mean)]
+        # column_variance_steps = [statistics.stdev(i) for i in zip(*num_steps_mean)]
+
+        plt.figure(1)
+        # plt.plot(num_episodes_list, column_average_steps, 'k')
+        plt.plot(num_episodes_list, column_average_steps, label=str(int(num_bins)))
+        # plt.fill_between(column_average_steps, list(map(float.__add__, column_average_steps, column_variance_steps)), list(map(float.__sub__, column_average_steps, column_variance_steps)), facecolor='blue', alpha=0.5)
+        # plt.errorbar(num_episodes_list, column_average_steps, yerr=column_variance_steps, elinewidth=0.75, linestyle='None')
+        plt.title("Learning curve")
+        plt.ylabel("Number of Steps")
+        plt.xlabel("Episodes")
+        # eight = mlines.Line2D([], [], color='c', marker='s', ls='', label='')
+        # nine = mlines.Line2D([], [], color='k', marker='s', ls='', label='mean')
+        # plt.legend(handles=[nine])
+        plt.legend()
+        # print("average time to reach target with learned policy = {}".format(mean(J)))
+    plt.show()
+
+
+
+    # best_threshold = None
+    # best_alpha = None
+    # best_eps = None
+    # best_sigma = None
+    # best_v = None
+    # best_n_iters = None
+    # best_max_norm = float("inf")
+    # for best_sigma in np.arange(1, 2, 1):
+    #     ps = PrioritizedSweeping(alpha=alpha, gamma=gamma, delta=delta, num_bins=num_bins)
+    #     count, num_episodes_list, num_actions_list, num_steps_list = ps.prio_sweep(eps=eps, threshold=threshold, n_iters=n_iters, break_iters=break_iters, running_average_length=running_average_length)
+    # #     if best_max_norm > max_norm:
+    # #         best_max_norm = max_norm
+    # #         best_threshold = threshold
+    # #         best_sigma = sigma
+    # #         best_alpha = alpha
+    # #         best_eps = eps
+    # #         best_v = ps.v
+    # #         best_n_iters = n_iters
+    # #         best_break_iters = break_iters
+    # #         ps.print_policy()
+    # #     # print(ps.v)
+    # # print("alpha = {}, eps = {}, threshold = {}, sigma= {}, n_iters = {}, break_iters = {} max_norm = {}".format(best_alpha, best_eps, best_threshold, best_sigma, best_n_iters, break_iters, best_max_norm))
+    # # print(best_v)
 
 
 if __name__ == '__main__':
